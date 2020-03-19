42ed382bd25975ccc0b0587d2fdfec251f8b4caa
Jiackie <Jiaqili1023@gmail.com>
2020-03-18 23:32:17 -0700

    remove the global function and add all of variable and function into a Object

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 
 This list page can help you to write and edit your list information.User can insert new list, edit exist list, delete the list or sort the list.
 
+### Features
+
+- Insert: insert content into the list;
+- Edit: edit the exist list;
+- Sort: sort the list in ascending or descending order;
+- Pagination: set the number of item in one page and switch;
+
 ### Function
 
 - insert function
@@ -16,3 +23,8 @@ This list page can help you to write and edit your list information.User can ins
 
 - pageSize: the number of items show within one page;
 - pageNum: the current page index;
+
+
+#### In process:
+- get the origin list data from API;
+- add Favorite feature to the list and show the favorite list.
--- list.js
+++ list.js
@@ -1,241 +1,207 @@
-let pageNum = 0;
-const pageSize = 10;
-let sortAscending = true;
+crudObj = {
+  pageNum: 0,
+  sortAscending: true,
+  pageSize: 10,
 
+  inputVal: document.getElementById('inputcontent'),
+  listNode: document.getElementById('list'),
+  updateVal: document.getElementById('updatedcontent'),
+  editDiv: document.getElementById('edit'),
+  orderChangeButton: document.getElementById('sort'),
+  pageNode: document.getElementById('pagination'),
+  editElement: null,
 
-
-function insertInList() {
-  //extract input value
-  let inputVal = document.getElementById('inputcontent');
-  let listNode = document.getElementById('list');
-
-  //check if input is NULL;
-  if (!inputVal.value.trim()) {
-    alert('Input cannot be empty! Please try again.');
-    return;
-  }
-
-  //creat a new li element
-  let newList = document.createElement('li');
-  let textNode = document.createTextNode(inputVal.value);
-  newList.appendChild(textNode);
-
-  //add delete button to it;
-  let delbutton = document.createElement('button');
-  delbutton.type = 'button';
-  delbutton.className = 'no';
-  delbutton.innerHTML = 'del';
-  // console.log(delbutton);
-  delbutton.onclick = function () {
-    deleteTheList();
-  }
-  newList.appendChild(delbutton);
-
-  //add edit button to it;
-  let editbutton = document.createElement('button');
-  editbutton.type = 'button';
-  editbutton.className = 'yes';
-  editbutton.innerHTML = 'edit';
-  editbutton.onclick = function() {
-    startEdit();
-  };
-  newList.appendChild(editbutton);
-
-  //sort to the right positon and insert;
-  sortAndInsert(newList);
-
-  //call pagination.
-  paginationBar();
-
-  //show the updated page;
-  showPageN(pageNum);
-
-  //remove content of input;
-  inputVal.value = "";
-}
-
-function editedTheList() {
-  //get the input value;
-  let inputVal = document.getElementById('updatedcontent').value;
-
-  //check the updated content is NULL;
-
-  if (!inputVal.trim()) {
-    alert('Updates cannot be empty! Please try again.');
-    return;
-  }
-  // console.log(editElement);
-  editElement.childNodes[0].textContent = inputVal;
-
-  //sort and insert into the specific position;
-  sortAndInsert(editElement);
-
-  showPageN(pageNum);
-
-  //hide the edit div;
-  let hideEditDiv = document.getElementById('edit');
-  hideEditDiv.style.display = 'none';
-
-}
-
-function cancelEdit() {
-  let hideEditDiv = document.getElementById('edit');
-  hideEditDiv.style.display = 'none';
-}
-
-//function for demo only;
-function startEdit() {
-  // to display the edit div
-  let showEditDiv = document.getElementById('edit');
-  showEditDiv.style.display = 'block';
-  // put the content in edit input box;
-  let setInputValue = document.getElementById('updatedcontent');
-  //get the position of the list that user want to edit;
-  editElement = event.srcElement.parentElement;
-  setInputValue.value = editElement.childNodes[0].textContent;
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-}
-
-//function for demo only;
-function deleteTheList() {
-  let deleteElement = event.srcElement.parentElement;
-  let listNode = document.getElementById('list');
-
-  deleteElement.remove();
-  paginationBar();
-
-  //check if need to change page;
-  if (pageNum === Math.ceil(listNode.children.length / pageSize)) {
-    pageNum = 0;
-  }
-  showPageN(pageNum);
-}
-
-function sortAndInsert(newItem) {
-  let orderlist = document.getElementById('list');
-  //compared with each value of the element;
-  console.log(sortAscending);
-  if (sortAscending) {
-    for (let llist of orderlist.children) {
-      if (newItem.childNodes[0].textContent.trim().toLowerCase() < llist.childNodes[0].textContent.trim().toLowerCase()) {
-        orderlist.insertBefore(newItem, llist);
-        return;
-      }
+  insertInList: function () {
+    //check if input is NULL;
+    if (!this.inputVal.value.trim()) {
+      alert('Input cannot be empty! Please try again.');
+      return;
     }
 
-  } else {
-    for (let llist of orderlist.children) {
-      if (newItem.childNodes[0].textContent.trim().toLowerCase() > llist.childNodes[0].textContent.trim().toLowerCase()) {
-        orderlist.insertBefore(newItem, llist);
-        return;
+    //creat a new li element
+    let newList = document.createElement('li');
+    newList.innerHTML = this.inputVal.value;
+    // let textNode = document.createTextNode(this.inputVal.value);
+    // newList.appendChild(textNode);
+
+    //add delete button to it;
+    let delbutton = document.createElement('button');
+    delbutton.type = 'button';
+    delbutton.className = 'no';
+    delbutton.innerHTML = 'del';
+    delbutton.onclick = () => crudObj.deleteTheList();
+    newList.appendChild(delbutton);
+
+    //add edit button to it;
+    let editbutton = document.createElement('button');
+    editbutton.type = 'button';
+    editbutton.className = 'yes';
+    editbutton.innerHTML = 'edit';
+    editbutton.onclick = () => crudObj.startEdit();
+    newList.appendChild(editbutton);
+
+    //sort to the right positon and insert;
+    this.sortAndInsert(newList);
+
+    //call pagination.
+    this.paginationBar();
+
+    //show the updated page;
+    this.showPageN(this.pageNum);
+
+    //remove content of input;
+    this.inputVal.value = '';
+  },
+
+  showPageN: function (pageNumber) {
+    for (let item of this.listNode.children) {
+      item.style.display = 'none';
+    }
+
+    for (let item = pageNumber * this.pageSize; item < (this.pageSize + pageNumber * this.pageSize); item ++) {
+      if (!this.listNode.children[item]) break;
+      this.listNode.children[item].style.display = 'list-item';
+    }
+
+    // if only one page, then only one button here;
+
+    for (let page = 0; page < this.pageNode.children.length - 1; page++) {
+      if (page === this.pageNum + 1) {
+        this.pageNode.children[page].style.backgroundColor = '#0229b5';
+      } else {
+        this.pageNode.children[page].style.backgroundColor = null;
       }
     }
-    orderlist.appendChild(newItem);
+  },
+
+  editedTheList: function () {
+    //check the updated content is NULL;
+
+    if (!this.updateVal.value.trim()) {
+      alert('Updates cannot be empty! Please try again.');
+      return;
+    }
+
+    this.editElement.childNodes[0].textContent = this.updateVal.value;
+
+    //sort and insert into the specific position;
+    this.sortAndInsert(this.editElement);
+
+    this.showPageN(this.pageNum);
+
+    //hide the edit div;
+    this.editDiv.style.display = 'none';
+  },
+
+  cancelEdit: function () {
+    this.editDiv.style.display = 'none';
+  },
+
+  startEdit: function () {
+    // to display the edit div
+    this.editDiv.style.display = 'block';
+
+    // put the content in edit input box;
+    //get the position of the list that user want to edit;
+    this.editElement = event.srcElement.parentElement;
+    this.updateVal.value = this.editElement.childNodes[0].textContent;
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  },
+
+  deleteTheList: function () {
+    this.editElement = event.srcElement.parentElement;
+    this.editElement.remove();
+    this.paginationBar();
+    this.showPageN(this.pageNum);
+  },
+
+  sortAndInsert: function (newItem) {
+
+    //compared with each value of the element;
+    if (this.sortAscending) {
+      for (let llist of this.listNode.children) {
+        if (newItem.childNodes[0].textContent.trim().toLowerCase() < llist.childNodes[0].textContent.trim().toLowerCase()) {
+          this.listNode.insertBefore(newItem, llist);
+          return;
+        }
+      }
+      this.listNode.appendChild(newItem);
+    } else {
+      for (let llist of this.listNode.children) {
+        if (newItem.childNodes[0].textContent.trim().toLowerCase() > llist.childNodes[0].textContent.trim().toLowerCase()) {
+          this.listNode.insertBefore(newItem, llist);
+          return;
+        }
+      }
+      this.listNode.appendChild(newItem);
+    }
+  },
+
+  reverseTheListOrder: function () {
+    if (this.orderChangeButton.textContent[0] === '\u2191') {
+      this.orderChangeButton.textContent = '\u2193A-Z';
+      this.sortAscending = false;
+    } else {
+      this.orderChangeButton.textContent = '\u2191A-Z';
+      this.sortAscending = true;
+    }
+
+    //reverse the order;
+    for (let i = 1, len = this.listNode.children.length; i < len; i++) {
+      this.listNode.insertBefore(this.listNode.children[i], this.listNode.firstChild);
+    }
+
+    this.showPageN(this.pageNum);
+  },
+
+  paginationBar: function () {
+    //remoce stored children of pagination;
+    this.pageNode.innerHTML = '';
+
+    //previous button
+    let firstButton = document.createElement('button');
+    firstButton.type = 'button';
+    firstButton.innerHTML = '<';
+    firstButton.onclick = function () {
+      if (crudObj.pageNum === 0) {
+        alert('This is the first page.');
+      } else {
+        crudObj.pageNum -= 1;
+        crudObj.showPageN(crudObj.pageNum);
+      }
+    };
+    this.pageNode.appendChild(firstButton);
+
+    //button for each page;
+    for (let i = 0, maxPage = Math.max(1,Math.ceil(this.listNode.children.length / this.pageSize)); i < maxPage; i++) {
+      let newButton = document.createElement('button');
+      newButton.type = 'button';
+      newButton.innerHTML = i + 1;
+      newButton.onclick = function () {
+        crudObj.pageNum = Number(newButton.innerHTML) - 1;
+        crudObj.showPageN(crudObj.pageNum);
+      };
+
+      this.pageNode.appendChild(newButton);
+    }
+
+    // next button;
+    let lastButton = document.createElement('button');
+    lastButton.type = 'button';
+    lastButton.innerHTML = '>';
+    lastButton.onclick = function () {
+      if (crudObj.pageNum === Math.ceil(crudObj.listNode.children.length / crudObj.pageSize) - 1 || crudObj.listNode.children.length === 0) {
+        alert('This is the last page.');
+      } else {
+        crudObj.pageNum += 1;
+        crudObj.showPageN(crudObj.pageNum);
+      }
+    };
+    this.pageNode.appendChild(lastButton);
   }
-
-}
-
-function reverseTheListOrder() {
-  let orderChangeButton = document.getElementById('sort');
-  if (orderChangeButton.textContent[0] === '\u2191') {
-    orderChangeButton.textContent = '\u2193A-Z';
-    sortAscending = false;
-  } else {
-    orderChangeButton.textContent = '\u2191A-Z';
-    sortAscending = true;
-    // orderChangeButton.textContent[0] = '\u2191';
-  };
-
-  //reverse the order;
-  let orderlist = document.getElementById('list');
-  for (let i = 1; i < orderlist.children.length; i++) {
-    // console.log(orderlist.firstChild);
-    orderlist.insertBefore(orderlist.children[i], orderlist.firstChild);
-  };
-  showPageN(pageNum);
-}
-
-//set pagination;
-window.onload = function () {
-  paginationBar();
-  showPageN(pageNum);
 };
 
-function paginationBar() {
-  let listNode = document.getElementById('list');
-  let pageNode = document.getElementById('pagination');
-
-  //remoce stored children of pagination;
-  pageNode.innerHTML = '';
-  // console.log(listNode.children.length);
-
-  //previous button
-  let firstButton = document.createElement('button');
-  firstButton.type = 'button';
-  firstButton.innerHTML = '<';
-  firstButton.onclick = function () {
-    if (pageNum === 0) {alert('This is the first page.');}
-    else {
-      pageNum -= 1;
-      showPageN(pageNum);
-    }
-  };
-  pageNode.appendChild(firstButton);
-
-  //button for each page;
-  for (let i = 0; i < Math.max(1,Math.ceil(listNode.children.length / pageSize)); i++) {
-    let newButton = document.createElement('button');
-    newButton.type = 'button';
-    newButton.innerHTML = i + 1;
-    newButton.onclick = function () {
-      pageNum = Number(newButton.innerHTML) - 1;
-      showPageN(pageNum);
-    };
-
-    pageNode.appendChild(newButton);
-  }
-
-  // next button;
-  let lastButton = document.createElement('button');
-  lastButton.type = 'button';
-  lastButton.innerHTML = '>';
-  lastButton.onclick = function () {
-    if (pageNum === Math.ceil(listNode.children.length / pageSize) - 1 || listNode.children.length === 0) {
-      alert('This is the last page.');
-    } else {
-      pageNum += 1;
-      showPageN(pageNum);
-    }};
-  pageNode.appendChild(lastButton);
-
-
-}
-
-
-
-function showPageN(pageNumber) {
-  let listNode = document.getElementById('list');
-
-  for (let item of listNode.children) {
-    item.style.display = 'none';
-  }
-
-  for (let item = pageNumber * pageSize; item < (pageSize + pageNumber * pageSize); item ++) {
-    if (!listNode.children[item]) break;
-    // console.log(listNode.children[item]);
-    listNode.children[item].style.display = 'list-item';
-  }
-
-  let pageNode = document.getElementById('pagination');
-  // if only one page, then only one button here;
-
-  for (let page = 0; page < pageNode.children.length - 1; page++) {
-    // console.log(pageNode.children[page],pageNode.children;
-    // console.log(pageNode.children[page]);
-    if (page === pageNum + 1) {
-      pageNode.children[page].style.backgroundColor = '#0229b5';
-    } else {
-      pageNode.children[page].style.backgroundColor = null;
-    }
-  }
-}
+window.onload = function () {
+  crudObj.paginationBar();
+  crudObj.showPageN(crudObj.pageNum);
+};
